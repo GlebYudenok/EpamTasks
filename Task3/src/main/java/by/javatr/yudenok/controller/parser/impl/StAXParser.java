878a488f15792d9c9281a13469722212f75d7181
user <gyudenok@mail.ru>
2019-06-07 04:24:54 +0300

    Add not working SAX parser test

--- Task3/src/main/java/by/javatr/yudenok/controller/parser/impl/StAXParser.java
+++ Task3/src/main/java/by/javatr/yudenok/controller/parser/impl/StAXParser.java
@@ -1,11 +1,20 @@
 package by.javatr.yudenok.controller.parser.impl;
 
-import by.javatr.yudenok.bean.Candy;
+import by.javatr.yudenok.bean.*;
 import by.javatr.yudenok.controller.parser.XMLParser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StAXParser implements XMLParser {
+
+    List<Candy> candyList = new ArrayList<>();
+    Ingredient ingredient = null;
+    CandyType candyType = null;
+    Producer producer = null;
+    Candy candy = null;
+    String content = null;
+    Value value = null;
 
     @Override
     public List<Candy> parseXML(String path) {
---
+++ Task3/src/test/java/by/javatr/yudenok/controller/parser/impl/SAXParsersTest.java
@@ -0,0 +1,42 @@
+package by.javatr.yudenok.controller.parser.impl;
+
+import by.javatr.yudenok.exception.ParserException;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SAXParsersTest {
+
+    public SAXParsers saxParsers = new SAXParsers();
+
+    List<String> paths = Arrays.asList("src\\test\\resources\\Candies.xml",
+            "src\\test\\resources\\EmptyFile.xml",
+            "src\\test\\resources\\WrongCandies.xml",
+            "wrongPath.xml");
+
+    @Test
+    public void parseIdeal() throws IOException, SAXException, ParserConfigurationException {
+        saxParsers.parseXML(paths.get(0)).forEach(System.out::println);
+    }
+
+    @Test(expected = ParserException.class)
+    public void parseEmptyFile() throws IOException, SAXException, ParserConfigurationException {
+        saxParsers.parseXML(paths.get(1));
+    }
+
+    @Test(expected = ParserException.class)
+    public void parseWrongFile() throws IOException, SAXException, ParserConfigurationException {
+        saxParsers.parseXML(paths.get(2));
+    }
+
+    @Test(expected = ParserException.class)
+    public void parseWrongPath() throws IOException, SAXException, ParserConfigurationException {
+        saxParsers.parseXML(paths.get(3));
+    }
+}
