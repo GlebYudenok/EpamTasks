58140acd37dd15e59fb86b81538c9201df3540e3
user <gyudenok@mail.ru>
2019-06-21 23:19:12 +0300

    add producer&consumer app

---
+++ DemoExampleMultithreading/src/main/java/by/javatr/yudenok/example8/Store.java
@@ -0,0 +1,25 @@
+package by.javatr.yudenok.example8;
+
+public class Store {
+    int counter = 0;
+    int N = 5;
+
+    synchronized int put(){
+        if(counter <= N){
+            counter++;
+            System.out.println("Store have " + counter + " tovar(ov)");
+            return 1;
+        }
+        return 0;
+    }
+
+    synchronized int get(){
+        if(counter > 0){
+            counter--;
+            System.out.println("Store have " + counter + " tovar(ov)");
+            return 1;
+        }
+        return 0;
+    }
+
+}
